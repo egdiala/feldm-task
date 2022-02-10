50ff73fccf1b3f996225f54ec685feda36cf979d
egdiala <dialaegwuchukwu@gmail.com>
2022-02-10 16:57:38 +0100

    Vite global component registration

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+//global component registration
+const components = import.meta.globEager('./components/*.vue')
+Object.entries(components).forEach(([path, definition]) => {
+  // Get name of component, based on filename
+  // "./components/Fruits.vue" will become "Fruits"
+  const componentName = path?.split('/')?.pop()?.replace(/\.\w+$/, '')
+
+  // Register component on this Vue instance
+  app.component(componentName!, definition.default)
+})
+
+
+app.mount('#app')
